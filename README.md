a6f5d43a8a18f15a1cd1cdf41ac5a3faaf1d593a
Roly Fentanes <roly426@gmail.com>
2011-07-23 13:09:12 -0700

    saving state

--- README.md
+++ README.md
@@ -1,52 +1,52 @@
 Usage
 ------------------
 
-    ircc = require('irc-colors');
+    c = require('irc-colors');
     ...
-    ircbot.say('#chan', ircc.blue('hello everyone')); // prints blue text
-    ircbot.say('#chan', ircc.underline.red('WARNING')); // can be chained
-    ircbot.say('#chan', ircc.white.black('inverted')); // white text with black background
+    ircbot.say('#chan', c.blue('hello everyone')); // prints blue text
+    ircbot.say('#chan', c.underline.red('WARNING')); // can be chained
+    ircbot.say('#chan', c.white.black('inverted')); // white text with black background
     
-    warn = ircc.bold.red.yellow;
+    warn = c.bold.red.yellow;
     ircbot.say('#chan', warn('BIGGER WARNING')); // bold red text with yellow background
-    ircbot.say('#chan', ircc.rainbow('having fun!'); // prints rainbow colored text
+    ircbot.say('#chan', c.rainbow('having fun!'); // prints rainbow colored text
 
-The style and extra functions can be used from the main *ircc* object or after another color, background color, or style. But background color functions must be used after a foreground color is used.
+The style and extra functions can be used from the main *c* object or after another color, background color, or style. But background color functions must be used after a foreground color is used.
 
 Colors
 --------------
-* white
-* black
-* blue (navy)
-* green
-* red
-* brown (maroon)
-* purple
-* orange (olive)
-* yellow
-* light green (lime)
-* teal (blue cyan)
-* light cyan (cyan) (aqua)
-* light blue (royal)
-* pink (light purple) (fuchsia)
-* grey
-* light grey (silver)
+* <span style="color:white;background-color:black">**white**</span>
+* <span style="color:black">**black**</span>
+* <span style="color:navy">**blue (navy)**</span>
+* <span style="color:green">**green**</span>
+* <span style="color:red">**red**</span>
+* <span style="color:brown">**brown (maroon)**</span>
+* <span style="color:purple">**purple (violet)**</span>
+* <span style="color:orange">**orange (olive)**</span>
+* <span style="color:yellow">**yellow**</span>
+* <span style="color:lime">**light green (lime)**</span>
+* <span style="color:teal">**teal (blue cyan)**</span>
+* <span style="color:aqua">**light cyan (cyan) (aqua)**</span>
+* <span style="color:blue">**light blue (royal)**</span>
+* <span style="color:fuchsia">**pink (light purple) (fuchsia)**</span>
+* <span style="color:grey">**grey**</span>
+* <span style="color:silver">**light grey (silver)**</span>
 
 Original name or alternate can be used, without spaces
 
-    bot.say('#chat', 'hi'.irc.bluecyan);
+    bot.say('#chat', c.bluecyan('hi'));
 
 
 Styles
 ------------
-* bold
-* underline
-* italic
+* **bold**
+* *italic*
+* <u>underline</u>
 
 
 Extras
 -----------
-* rainbow
+* **<span style="color:red">r</span><span style="color:orange">a</span><span style="color:yellow">i</span><span style="color:green">n</span><span style="color:blue">b</span><span style="color:navy">o</span><span style="color:purple">w</span>**
 
 
 Installation
--- irc-colors.js
+++ irc-colors.js
@@ -1,5 +1,5 @@
 (function() {
-  var addingObj, bg, bgColorFuncs, c, color, colorArr, colorFuncs, colors, extraFuncs, f, fg, i, irc, name, obj, style, styleFuncs, styles, _i, _j, _k, _l, _len, _len2, _len3, _len4, _len5, _m, _ref, _ref2, _ref3;
+  var addingObj, bg, bgColorFuncs, bgF, c, color, colorArr, colorF, colorFuncs, colors, extraFuncs, f, fg, i, name, obj, style, styleF, styleFuncs, styles, _i, _j, _k, _l, _len, _len2, _len3, _len4, _len5, _m, _ref, _ref2, _ref3;
   colors = {
     '00': ['white'],
     '01': ['black'],
@@ -18,43 +18,57 @@
     '14': ['grey'],
     '15': ['lightgrey', 'silver']
   };
-  colorFuncs = {};
+  styles = {
+    '\\x00': 'normal',
+    '\\x01': 'underline',
+    '\\x02': 'bold',
+    '\\x16': 'italic'
+  };
+  bgColorFuncs = {};
   for (i in colors) {
     colorArr = colors[i];
     for (_i = 0, _len = colorArr.length; _i < _len; _i++) {
       color = colorArr[_i];
-      colorFuncs[color] = function(str) {
-        return "\x03" + i + str + "\x03";
-      };
+      bgColorFuncs[color] = (function(i) {
+        return function(str) {
+          return str.charAt(1) + ',' + i + str.substr(1);
+        };
+      })(i);
     }
   }
-  bgColorFuncs = {};
+  styleFuncs = {};
+  for (c in styles) {
+    style = styles[c];
+    styleFuncs[style] = (function(c) {
+      return function(str) {
+        return "" + c + str + c;
+      };
+    })(c);
+  }
+  colorFuncs = {};
   for (i in colors) {
     colorArr = colors[i];
     for (_j = 0, _len2 = colorArr.length; _j < _len2; _j++) {
       color = colorArr[_j];
-      bgColorFuncs[color] = function(str) {
-        return str.charAt(1) + ',' + i + str.substr(1);
-      };
+      colorFuncs[color] = (function(i) {
+        return function(str) {
+          return "\\x03" + i + str + "\\x03";
+        };
+      })(i);
+      /*
+          for j, bgArr of colors
+            for bg in bgArr
+              colorFuncs[color][bg] = ((j) ->
+                (str) ->
+                  "\xx03#{i},#{j}#{str}\\x03"
+              )(j)
+          */
     }
-  }
-  styles = {
-    '\x00': 'normal',
-    '\x01': 'underline',
-    '\x02': 'bold',
-    '\x16': 'italic'
-  };
-  styleFuncs = {};
-  for (c in styles) {
-    style = styles[c];
-    styleFuncs[style] = function(str) {
-      return "" + c + str + c;
-    };
   }
   extraFuncs = {
     rainbow: function(str) {
       var c, rainbowColors, s, _k, _len3, _ref;
-      rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'lightblue', 'violet'];
+      rainbowColors = ['red', 'orange', 'yellow', 'green', 'lightblue', 'navy', 'violet'];
       i = 0;
       s = '';
       _ref = str.split('');
@@ -68,32 +82,62 @@
     }
   };
   for (fg in colorFuncs) {
+    colorF = colorFuncs[fg];
     for (bg in bgColorFuncs) {
-      f = bgColorFuncs[bg];
-      fg[bg] = f;
+      bgF = bgColorFuncs[bg];
+      colorFuncs[fg][bg] = (function(bgF, colorF) {
+        return function(str) {
+          return bgF(colorF(str));
+        };
+      })(bgF, colorF);
     }
   }
   _ref = [colorFuncs, bgColorFuncs];
   for (_k = 0, _len3 = _ref.length; _k < _len3; _k++) {
     obj = _ref[_k];
     for (color in obj) {
+      colorF = obj[color];
       _ref2 = [styleFuncs, extraFuncs];
       for (_l = 0, _len4 = _ref2.length; _l < _len4; _l++) {
         addingObj = _ref2[_l];
         for (style in addingObj) {
-          f = addingObj[style];
-          obj[color][style] = f;
+          styleF = addingObj[style];
+          obj[color][style] = (function(styleF, colorF) {
+            return function(str) {
+              return styleF(colorF(str));
+            };
+          })(styleF, colorF);
         }
       }
     }
   }
-  module.exports = irc = {};
+  for (style in styleFuncs) {
+    styleF = styleFuncs[style];
+    for (color in colorFuncs) {
+      colorF = colorFuncs[color];
+      styleFuncs[style][color] = (function(styleF, colorF) {
+        return function(str) {
+          return colorF(styleF(str));
+        };
+      })(styleF, colorF);
+    }
+  }
+  module.exports = c = {};
   _ref3 = [colorFuncs, styleFuncs, extraFuncs];
   for (_m = 0, _len5 = _ref3.length; _m < _len5; _m++) {
     obj = _ref3[_m];
     for (name in obj) {
       f = obj[name];
-      irc[name] = f;
+      c[name] = (function(f) {
+        return f;
+      })(f);
     }
   }
+  console.log(c.blue.white('time'));
+  console.log(c.blue('blue'));
+  console.log(c.bold('bold'));
+  console.log(c.bold.grey('bold grey'));
+  console.log(c.green.underline('green underline'));
+  console.log(c.bold.white.black('bold white black'));
+  console.log(c.white.black.italic('white black italic'));
 }).call(this);
