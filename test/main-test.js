83d67a0b8b67a9d422e4b27adfc0e30539f65de3
Roly Fentanes <roly426@gmail.com>
2012-10-11 15:36:53 -0700

    some cleanup

--- test/main-test.js
+++ test/main-test.js
@@ -1,6 +1,6 @@
-var vows   = require('vows')
-  , assert = require('assert')
-  ,      c = require('..')
+var vows   = require('vows');
+var assert = require('assert');
+var      c = require('..');
 
 // activate global syntax
 // modifies the String prototype for a sugary syntax
@@ -14,15 +14,19 @@ var topicMacro = function(reg) {
       var obj = {};
 
       for (var key in tests) {
-        var fn = reg ? c : tests[key][0].irc
-          , s = key.split('.')
-          ;
+        if (tests.hasOwnProperty(key)) {
+          var fn = reg ? c : tests[key][0].irc
+            , s = key.split('.')
+            ;
 
-        for (var i in s) {
-          fn = fn[s[i]];
+          for (var i in s) {
+            if (s.hasOwnProperty(i)) {
+              fn = fn[s[i]];
+            }
+          }
+
+          obj[key] = reg ? fn(tests[key][0]) : fn();
         }
-
-        obj[key] = reg ? fn(tests[key][0]) : fn();
       }
       return obj;
     }
@@ -30,31 +34,35 @@ var topicMacro = function(reg) {
 };
 
 var tests = {
-  'blue'               : [txt, '12'       + txt + ''],
-  'white'              : [txt, '00'       + txt + ''],
-  'blue.white'         : [txt, '0012'   + txt + ''],
-  'bold'               : [txt, ''         + txt + ''],
-  'bold.grey'          : [txt, '14'     + txt + ''],
-  'green.underline'    : [txt, '03'     + txt + ''],
-  'bold.white.black'   : [txt, '0100' + txt + ''],
-  'white.black.italic' : [txt, '0100' + txt + ''],
-  'bggray'             : [txt, '01,14'    + txt + ''],
-  'rainbow'            : ['hello', '04h07e08l03l12o']
+  'blue'               : [txt, '\x0312'           + txt + '\x03'],
+  'white'              : [txt, '\x0300'           + txt + '\x03'],
+  'blue.white'         : [txt, '\x0300\x0312'     + txt + '\x03\x03'],
+  'bold'               : [txt, '\x02'             + txt + '\x02'],
+  'bold.grey'          : [txt, '\x0314\x02'       + txt + '\x02\x03'],
+  'underline'          : [txt, '\x01'             + txt + '\x01'],
+  'green.underline'    : [txt, '\x01\x0303'       + txt + '\x03\x01'],
+  'bold.white.black'   : [txt, '\x0301\x0300\x02' + txt + '\x02\x03\x03'],
+  'white.black.italic' : [txt, '\x16\x0301\x0300' + txt + '\x03\x03\x16'],
+  'bggray'             : [txt, '\x0301,14'        + txt + '\x03'],
+  'rainbow'            : ['hello', '\x0304h\x03\x0307e\x03\x0308l\x03\x0303l\x03\x0312o\x03']
 };
 
-var regular = topicMacro(true)
-  , globalSyntax = topicMacro(false)
+var regular = topicMacro(true);
+var globalSyntax = topicMacro(false);
 
+function test(key) {
+  regular[key] = function(topic) {
+    assert.equal(topic[key], tests[key][1]);
+  };
+  globalSyntax[key] = function(topic) {
+    assert.equal(topic[key], tests[key][1]);
+  };
+}
 
 for (var key in tests) {
-  (function(key) {
-    regular[key] = function(topic) {
-      assert.equal(topic[key], tests[key][1]);
-    };
-    globalSyntax[key] = function(topic) {
-      assert.equal(topic[key], tests[key][1]);
-    };
-  })(key);
+  if (tests.hasOwnProperty(key)) {
+    test(key);
+  }
 }
 
 vows.describe('Test').addBatch({
