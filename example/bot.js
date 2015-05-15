c90b7804757de7a85a8109cb37fff3a1e78b6938
Roly Fentanes <roly426@gmail.com>
2015-05-15 10:48:35 -0400

    bot tests a few more things

--- example/bot.js
+++ example/bot.js
@@ -1,12 +1,14 @@
 var irc = require('irc');
-var c = require('./');
+var c = require('../');
 
-var channel = '#rolytest1234';
-var client = new irc.Client('irc.freenode.com', 'rolybot1234', {
+var channel = '#rolytest';
+var client = new irc.Client('chat.freenode.net', 'rolybot1234', {
   channels: [channel]
 });
 
 client.on('message' + channel, function (from, message) {
-  client.say(channel, 'echo ' + c.underline(message));
+  client.say(channel, 'underline: ' + c.underline(message));
+  client.say(channel, 'bold:      ' + c.bold(message));
+  client.say(channel, 'italic:    ' + c.italic(message));
 });
 
