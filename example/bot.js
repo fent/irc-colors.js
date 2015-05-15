3fa28cf0461c59e2645327bbe7f01db4201a380b
fent <933490+fent@users.noreply.github.com>
2015-05-15 10:48:35 -0400

    bot tests a few more things

--- example/bot.js
+++ example/bot.js
@@ -1,12 +1,14 @@
 var irc = require('irc');
-var c = require('./');
+var c = require('../');
 
-var channel = '#colortest1234';
-var client = new irc.Client('irc.freenode.com', 'colorbot1234', {
+var channel = '#colortest';
+var client = new irc.Client('chat.freenode.net', 'colorbot1234', {
   channels: [channel]
 });
 
 client.on('message' + channel, function (from, message) {
-  client.say(channel, 'echo ' + c.underline(message));
+  client.say(channel, 'underline: ' + c.underline(message));
+  client.say(channel, 'bold:      ' + c.bold(message));
+  client.say(channel, 'italic:    ' + c.italic(message));
 });
 
