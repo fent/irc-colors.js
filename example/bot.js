c9efc2cf4d0271be42057f6fece014039236330c
fent <933490+fent@users.noreply.github.com>
2016-03-16 12:37:31 -0400

    have bot echo color

--- example/bot.js
+++ example/bot.js
@@ -2,7 +2,7 @@ var irc = require('irc');
 var c = require('../');
 
 var channel = '#rolytest';
-var client = new irc.Client('chat.freenode.net', 'rolybot1234', {
+var client = new irc.Client('bethere.fdfnet.net', 'rolybot1234', {
   channels: [channel]
 });
 
@@ -10,5 +10,6 @@ client.on('message' + channel, function (from, message) {
   client.say(channel, 'underline: ' + c.underline(message));
   client.say(channel, 'bold:      ' + c.bold(message));
   client.say(channel, 'italic:    ' + c.italic(message));
+  client.say(channel, 'red:       ' + c.red(message));
 });
 
