67e6b19e9c84d6a3256463ab3068c18df04defc7
fent <933490+fent@users.noreply.github.com>
2018-07-22 01:02:00 -0400

    docs(example): add rainbow message

--- example/bot.js
+++ example/bot.js
@@ -2,7 +2,7 @@ const irc = require('irc');
 const c = require('../');
 
 let channel = '#colortest';
-let client = new irc.Client('bethere.fdfnet.net', 'colorbot1234', {
+let client = new irc.Client('irc.uk.mibbit.net', 'colorbot1234', {
   channels: [channel]
 });
 
@@ -11,5 +11,6 @@ client.on('message' + channel, (from, message) => {
   client.say(channel, 'bold:      ' + c.bold(message));
   client.say(channel, 'italic:    ' + c.italic(message));
   client.say(channel, 'red:       ' + c.red(message));
+  client.say(channel, 'rainbow:   ' + c.rainbow(message));
 });
 
